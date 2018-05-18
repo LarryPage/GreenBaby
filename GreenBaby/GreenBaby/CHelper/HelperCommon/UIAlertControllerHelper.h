7f43346310380c08cb190305bff5cbeee5801f2b
LarryPage <sbtjfdn@hotmail.com>
2018-05-18 11:28:36 +0800

    【MODIFY】UIAlertController category

--- GreenBaby/GreenBaby/CHelper/HelperCommon/UIAlertControllerHelper.h
+++ GreenBaby/GreenBaby/CHelper/HelperCommon/UIAlertControllerHelper.h
@@ -1,9 +1,9 @@
 //
 //  UIAlertControllerHelper.h
-//  GreenBaby
+//  BrcIot
 //
 //  Created by LiXiangCheng on 2017/10/12.
-//  Copyright © 2017年 LiXiangCheng. All rights reserved.
+//  Copyright © 2018年 BRC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -11,6 +11,13 @@
 typedef void (^alertActionHandler)(UIAlertAction *action);
 
 @interface UIAlertController (Helper)
+/**
+ * @brief 是否已存在UIAlertController
+ */
+- (BOOL)isExistAlertController;
+@end
+
+@interface UIAlertController (UIAlertView)
 /*
  * 方便快捷的方法抛出一个警告用户
  * bTitle_ 为空默认"Cancel"
@@ -20,8 +27,8 @@ typedef void (^alertActionHandler)(UIAlertAction *action);
 /**
  * @brief 便利方法，可以把点击按钮的触发事件卸载block里
  UIAlertActionStyleCancel:对按钮应用取消样式，即取消操作
- UIAlertActionStyleDefault:对按钮应用标准样式:确定
- UIAlertActionStyleDestructive:对按钮应用警示性样式，提示用户这样做可能会删除或者改变某些数据
+ UIAlertActionStyleDefault:对按钮应用标准样式:确定,可建多个
+ UIAlertActionStyleDestructive:对按钮应用警示性样式，提示用户这样做可能会删除或者改变某些数据，会显示红色
  */
 + (void)showWithTitle:(NSString *)title
               message:(NSString *)message
@@ -31,9 +38,25 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
              onCancel:(alertActionHandler)cancelHander
              onDefult:(alertActionHandler)defaultHander
         onDestructive:(alertActionHandler)destructiveHander;
+@end
 
+@interface UIAlertController (UIActionSheet)
 /**
- * @brief 是否已存在UIAlertController
+ * @brief 便利方法，可以把点击按钮的触发事件卸载block里
+ UIAlertActionStyleCancel:对按钮应用取消样式，即取消操作
+ UIAlertActionStyleDefault:对按钮应用标准样式:确定，可建多个
+ UIAlertActionStyleDestructive:对按钮应用警示性样式，会显示红色
+ sourceView:要在此视图显示的UIActionSheet
  */
-- (BOOL)isExistAlertController;
++ (void)showWithTitle:(NSString *)title
+              message:(NSString *)message
+    cancelButtonTitle:(NSString *)cancelButtonTitle
+   defultButton1Title:(NSString *)defultButton1Title
+   defultButton2Title:(NSString *)defultButton2Title
+destructiveButtonTitle:(NSString *)destructiveButtonTitle
+             onCancel:(alertActionHandler)cancelHander
+            onDefult1:(alertActionHandler)default1Hander
+            onDefult2:(alertActionHandler)default2Hander
+        onDestructive:(alertActionHandler)destructiveHander
+           sourceView:(UIView *)sourceView;
 @end
--- GreenBaby/GreenBaby/CHelper/HelperCommon/UIAlertControllerHelper.m
+++ GreenBaby/GreenBaby/CHelper/HelperCommon/UIAlertControllerHelper.m
@@ -10,6 +10,21 @@
 
 @implementation UIAlertController (Helper)
 
+- (BOOL)isExistAlertController{
+    for (UIWindow* window in [UIApplication sharedApplication].windows) {
+        for (UIView* view in window.subviews) {
+            BOOL alert = [view isKindOfClass:[UIAlertController class]];
+            if (alert)
+                return YES;
+        }
+    }
+    return NO;
+}
+
+@end
+
+@implementation UIAlertController (UIAlertView)
+
 + (void)alert:(NSString *)message_ title:(NSString *)title_ bTitle:(NSString *)bTitle_
 {
     if (bTitle_ == nil) {
@@ -34,6 +49,7 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
         onDestructive:(alertActionHandler)destructiveHander
 {
     UIAlertController *alert=[UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
     if (cancelButtonTitle && cancelButtonTitle.length) {
         //UIAlertActionStyleCancel:对按钮应用取消样式，即取消操作
         UIAlertAction *cancel=[UIAlertAction actionWithTitle:cancelButtonTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction *action){
@@ -41,6 +57,8 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
         }];
         [alert addAction:cancel];
     }
+    
+    //defaultButton可多个
     if (defultButtonTitle && defultButtonTitle.length) {
         //UIAlertActionStyleDefault:对按钮应用标准样式:确定
         UIAlertAction *defult = [UIAlertAction actionWithTitle:defultButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
@@ -48,6 +66,7 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
         }];
         [alert addAction:defult];
     }
+    
     if (destructiveButtonTitle && destructiveButtonTitle.length) {
         //UIAlertActionStyleDestructive:对按钮应用警示性样式，提示用户这样做可能会删除或者改变某些数据
         UIAlertAction *destructive = [UIAlertAction actionWithTitle:destructiveButtonTitle style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action){
@@ -55,18 +74,66 @@ destructiveButtonTitle:(NSString *)destructiveButtonTitle
         }];
         [alert addAction:destructive];
     }
+    
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
-- (BOOL)isExistAlertController{
-    for (UIWindow* window in [UIApplication sharedApplication].windows) {
-        for (UIView* view in window.subviews) {
-            BOOL alert = [view isKindOfClass:[UIAlertController class]];
-            if (alert)
-                return YES;
-        }
+@end
+
+@implementation UIAlertController (UIActionSheet)
+
++ (void)showWithTitle:(NSString *)title
+              message:(NSString *)message
+    cancelButtonTitle:(NSString *)cancelButtonTitle
+   defultButton1Title:(NSString *)defultButton1Title
+   defultButton2Title:(NSString *)defultButton2Title
+destructiveButtonTitle:(NSString *)destructiveButtonTitle
+             onCancel:(alertActionHandler)cancelHander
+            onDefult1:(alertActionHandler)default1Hander
+            onDefult2:(alertActionHandler)default2Hander
+        onDestructive:(alertActionHandler)destructiveHander
+           sourceView:(UIView *)sourceView;{
+    UIAlertController *alert=[UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    if (cancelButtonTitle && cancelButtonTitle.length) {
+        //UIAlertActionStyleCancel:对按钮应用取消样式，即取消操作
+        UIAlertAction *cancel=[UIAlertAction actionWithTitle:cancelButtonTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction *action){
+            cancelHander(action);
+        }];
+        [alert addAction:cancel];
     }
-    return NO;
+    
+    //defaultButton可多个
+    if (defultButton1Title && defultButton1Title.length) {
+        //UIAlertActionStyleDefault:对按钮应用标准样式:确定
+        UIAlertAction *defult1 = [UIAlertAction actionWithTitle:defultButton1Title style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+            default1Hander(action);
+        }];
+        [alert addAction:defult1];
+    }
+    if (defultButton2Title && defultButton2Title.length) {
+        //UIAlertActionStyleDefault:对按钮应用标准样式:确定
+        UIAlertAction *defult2 = [UIAlertAction actionWithTitle:defultButton2Title style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+            default2Hander(action);
+        }];
+        [alert addAction:defult2];
+    }
+    
+    if (destructiveButtonTitle && destructiveButtonTitle.length) {
+        //UIAlertActionStyleDestructive:对按钮应用警示性样式，提示用户这样做可能会删除或者改变某些数据
+        UIAlertAction *destructive = [UIAlertAction actionWithTitle:destructiveButtonTitle style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action){
+            destructiveHander(action);
+        }];
+        [alert addAction:destructive];
+    }
+    
+    [alert setModalPresentationStyle:UIModalPresentationPopover];
+    
+    UIPopoverPresentationController *popover = [alert popoverPresentationController];
+    popover.sourceView = sourceView;
+    popover.sourceRect = sourceView.bounds;
+    popover.permittedArrowDirections = UIPopoverArrowDirectionAny;
+    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
